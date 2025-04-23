a54dd78141b649fd89b44baa6a998a0722fbf88d
Igberry <davidigberichuks@gmail.com>
2025-04-23 13:32:55 +0100

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-const contactRoutes = require('./routes/contactRoutes');
+const contactRoutes = require('./backend/routes/contactRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
